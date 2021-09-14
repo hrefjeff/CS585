fef3b8e11a781b0ae3d063bb54e3f3c531bf88ab
Jeffrey Allen <jeffwayne256@gmail.com>
2021-09-13 19:47:35 -0500

    Working encryption and added scratchpad

--- VigenereCipher/main.py
+++ VigenereCipher/main.py
@@ -2,25 +2,49 @@
 
 # Global variables
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-input_string = ""
+plain_text = ""
 key = ""
 expanded_key = ""
 encrypted_string = ""
 decrypted_string = ""
+i = 0
 
 # Enter key here
 key = "Vig"
 key = key.lower()
 
 # Enter the string that needs to be encrypted here
-input_string = "THE BOY HAS THE BALL"
-input_string = input_string.lower()
+plain_text = "THE BOY HAS THE BALL"
+plain_text = plain_text.lower()
 
 # Expand the key in the case it is shorter than the string to be encrypted
 expanded_key = key
 expanded_key_length = len(expanded_key)
-input_string_length = len(input_string)
+plain_text_length = len(plain_text)
 
-while expanded_key_length < input_string_length:
+while expanded_key_length < plain_text_length:
     expanded_key = expanded_key + key
     expanded_key_length = len(expanded_key)
+
+# Encrypt each letter
+for letter in plain_text:
+    if letter in alphabet:
+        index = alphabet.find(letter) # get numerical representation of plain text letter
+        key_char = expanded_key[i] # get numerical representation of key letter
+        i += 1
+        key_index = ord(key_char) - 97 # 97 to get 0 index
+
+        # enciphered index = (plaintext index + keyword index) mod 26
+        encrypted_index = (index + key_index) % 26
+
+        encrypted_char = alphabet[encrypted_index]
+
+        encrypted_string = encrypted_string + encrypted_char
+
+
+print("-------------------")
+print("Vigenere Cipher")
+print("-------------------")
+print("Plain text: " + plain_text)
+print("Encrypted text: " + encrypted_string)
+print("Decrypted text: " + decrypted_string + "\n")
---
+++ VigenereCipher/scratchpad.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+letter = "z"
+
+index = alphabet.find(letter)
+
+print(index)
+
+print(ord(letter) - 97)
