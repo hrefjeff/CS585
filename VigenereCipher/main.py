c76d852b75515218e1582bac76bc9fff0950eeba
Jeffrey Allen <jeffwayne256@gmail.com>
2021-09-16 11:15:39 -0500

    Moved ecnrypt to function

--- VigenereCipher/main.py
+++ VigenereCipher/main.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
 
-# Global variables
+# Variables and Setup
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-plain_text = ""
-key = ""
-expanded_key = ""
-encrypted_string = ""
-decrypted_string = ""
-i = 0
-
-# Enter key here
-key = "Vig"
-key = key.lower()
-
-# Enter the string that needs to be encrypted here
 plain_text = "THE BOY HAS THE BALL"
+key = "Vig"
+
 plain_text = plain_text.lower()
+key = key.lower()
 
 # Expand the key in the case it is shorter than the string to be encrypted
 expanded_key = key
@@ -26,25 +17,36 @@ while expanded_key_length < plain_text_length:
     expanded_key = expanded_key + key
     expanded_key_length = len(expanded_key)
 
-# Encrypt each letter
-for letter in plain_text:
-    if letter in alphabet:
-        index = alphabet.find(letter) # get numerical representation of plain text letter
-        key_char = expanded_key[i] # get numerical representation of key letter
-        i += 1
-        key_index = ord(key_char) - 97 # 97 to get 0 index
+decrypted_string = ""
 
-        # enciphered index = (plaintext index + keyword index) mod 26
-        encrypted_index = (index + key_index) % 26
+# Enter the string that needs to be encrypted here
+def vigenere_encrypt(plain_text):
 
-        encrypted_char = alphabet[encrypted_index]
+    plain_text = plain_text.lower()
+    key_pos = 0
+    encrypted_string = ""
 
-        encrypted_string = encrypted_string + encrypted_char
+    # Encrypt each letter
+    for letter in plain_text:
+        if letter in alphabet:
+            index = alphabet.find(letter) # get numerical representation of plain text letter
+            key_char = expanded_key[key_pos] # get numerical representation of key letter
+            key_pos += 1
+            key_index = ord(key_char) - 97 # 97 to get 0 index
+
+            # enciphered index = (plaintext index + keyword index) mod 26
+            encrypted_index = (index + key_index) % 26
+
+            encrypted_char = alphabet[encrypted_index]
+
+            encrypted_string += encrypted_char
+
+    return encrypted_string
 
 
 print("-------------------")
 print("Vigenere Cipher")
 print("-------------------")
 print("Plain text: " + plain_text)
-print("Encrypted text: " + encrypted_string)
+print("Encrypted text: " + vigenere_encrypt(plain_text))
 print("Decrypted text: " + decrypted_string + "\n")
